8823f686abc88971fc1062d0312ca8089257d2ba
Alex <Alexvvv9511@yandex.ru>
2024-09-17 22:18:00 +0300

    sprint 9 commit2

--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -2,6 +2,7 @@ package com.example.playlistmaker
 
 import android.annotation.SuppressLint
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.widget.Button
 import androidx.activity.enableEdgeToEdge
@@ -21,5 +22,29 @@ class SettingsActivity : AppCompatActivity() {
             startActivity(backToActivityMainButtonIntent)
         }
 
+        val shareAppButton = findViewById<Button>(R.id.share_app_button)
+        shareAppButton.setOnClickListener {
+            val shareAppButtonIntent = Intent(Intent.ACTION_SENDTO)
+            shareAppButtonIntent.data = Uri.parse("mailto:")
+            shareAppButtonIntent.putExtra(Intent.EXTRA_TEXT,getString(R.string.android_developer))
+            startActivity(shareAppButtonIntent)
+        }
+
+        val writeToSupportButton = findViewById<Button>(R.id.write_to_support_button)
+        writeToSupportButton.setOnClickListener {
+            val writeToSupportButtonIntent = Intent(Intent.ACTION_SENDTO)
+            writeToSupportButtonIntent.data = Uri.parse("mailto:")
+            writeToSupportButtonIntent.putExtra(Intent.EXTRA_EMAIL,getString(R.string.my_mail_address))
+            writeToSupportButtonIntent.putExtra(Intent.EXTRA_SUBJECT,getString(R.string.title_mail))
+            writeToSupportButtonIntent.putExtra(Intent.EXTRA_TEXT,getString(R.string.body_of_mail))
+            startActivity(writeToSupportButtonIntent)
+        }
+
+        val userAgreementButton = findViewById<Button>(R.id.user_agreement_button)
+        userAgreementButton.setOnClickListener {
+            val userAgreementButtonIntent = Intent(Intent.ACTION_VIEW)
+            userAgreementButtonIntent.setData(Uri.parse(getString(R.string.link_user_agreement)))
+            startActivity(userAgreementButtonIntent)
+        }
     }
 }
